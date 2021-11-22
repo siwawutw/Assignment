46cb1c52645bd3b08cf8a9f806e305bed118c94f
57011252 <siwawutw@oic.or.th>
2021-11-22 12:07:25 +0700

    commit

--- loginsystem/urls.py
+++ loginsystem/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import index,register
+from .views import index, logout,register,login
 urlpatterns = [
     path('member',index,name="member"),
     path('register/add',register,name="addUser"),
+    path('login',login,name="login"),
+    path('logout',logout,name="logout"),
 ]
--- writerpanel/views.py
+++ writerpanel/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from blogs.models import Blogs
-from django.db.models import Sum
+from django.db.models import Sum #สำหรับใช้งาน sum หายอดวิวรวม
+from django.contrib.auth.decorators import login_required #ใช้งานการ login
+from django.contrib.auth.models import auth
 # Create your views here.
+
+@login_required(login_url="member") #จะเรียกใช้ function panel ต้อง log in ก่อน 
+
 def panel(request):
-    writer = "admin"
+    writer = auth.get_user(request)
     blogs = Blogs.objects.filter(writer=writer)
     blogCount = blogs.count()
     total = Blogs.objects.filter(writer=writer).aggregate(Sum("views")) #การหายอด view รวม
